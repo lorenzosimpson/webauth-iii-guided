658e66d71eb19bcacfe2390a96a29e9b8d27347e
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-13 12:44:17 -0500

    adds token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getJwtToken(user.username);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +39,15 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function getJwtToken(username) {
+  const payload = { username, roles: 'student'};
+  const secret = process.env.JWT_SECRET || 'is it secret, is it safe?';
+  const options = {
+    expiresIn: '1d',
+  }
+
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
